9f340831005d4f7e0efc6a9d816dda2fef7e8eb9
Aleksandr Samrinov <alesam@yandex.ru>
2025-10-27 17:48:52 +0300

    1

--- anfisa_for_friends/homepage/urls.py
+++ anfisa_for_friends/homepage/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = 'homepage'
+app_name = "homepage"
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
 ]
